49bd967c85b82a20a446d3ab54cb37cfe0b6537b
shubham.mishra <shubham.mishra@enercross.com>
2024-07-09 23:50:52 +0530

    changes to resolve deploying error

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,52 @@
-import express from "express"
-import mongoose from "mongoose"
-import authRouter from './routes/auth.route.js'
-import userRouter from './routes/user.route.js'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import listingRouter from './routes/listing.route.js'
-import gmailRouter from './routes/gmail.route.js'
-import path from 'path'
-import dotenv from 'dotenv'
+import express from "express";
+import mongoose from "mongoose";
+import authRouter from './routes/auth.route.js';
+import userRouter from './routes/user.route.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js';
+import gmailRouter from './routes/gmail.route.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 dotenv.config();
 
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to database");
+});
 
+// This is needed to resolve __dirname in ES6 modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("connected to database")
-})
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-const __dirname = path.resolve();
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
+app.use('/api/listing', listingRouter);
+app.use('/api/mail', gmailRouter); // ****** IGNORE THIS ROUTE **************** ////////////////////////////
 
-app.use('/api/auth',authRouter)
-app.use('/api/user',userRouter)
-app.use('/api/listing',listingRouter)
-app.use('/api/mail',gmailRouter) // ****** IGNORE THIS ROUTE **************** ////////////////////////////
-
-
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-})
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
 
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal serveer issue";
+    const message = err.message || "Internal server issue";
     res.status(statusCode).send({
-        success:false,
+        success: false,
         message,
         statusCode
-    })
-})
-app.listen(process.env.PORT,()=>{
-    console.log("app is listenimg at port "+process.env.PORT)
-})
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("App is listening at port " + process.env.PORT);
+});
