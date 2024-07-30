48020a4750908aea5cddb1b2bfc5022f44a36071
shubham.mishra <shubham.mishra@enercross.com>
2024-07-31 03:45:33 +0530

    title

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -32,15 +32,15 @@ export default function Home() {
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+          Find your next <span className='text-slate-500'>favourite</span>
           <br />
-          place with ease
+          food with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Our Estate is the best place to find your next perfect place to
-          live.
+          Delish Drive is the best food delivery platform to find your next favourite food to
+          have.
           <br />
-          We have a wide range of properties for you to choose from.
+          We have a wide range of varieties for you to choose from.
         </div>
         <Link
           to={'/search'}
